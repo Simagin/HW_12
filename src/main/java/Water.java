1702f6f9ae3b86379c9f707fb93ad2d230aabe5e
”Simagingit config --global user.email simagin090@gmail.comgit config --global user.name ”Simagin <simagin090@gmail.com>
2021-09-01 12:00:54 +0300

    hw-12.2

--- src/main/java/Water.java
+++ src/main/java/Water.java
@@ -4,19 +4,21 @@ import java.util.concurrent.Phaser;
 
 public class Water {
 
-    Phaser phaser = new Phaser(3);
+    Phaser Phaser = new Phaser(4);
+    static String INPUUT = "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH";
 
     public static void main(String[] args) {
+
         new Water().makeWater();
     }
 
     private void makeWater() {
-        for (int phase = 0; phase < 3; phase++) {
-            new Hydrogen(phaser);
-            new Hydrogen(phaser);
-            new Oxygen(phaser);
-            phaser.arriveAndAwaitAdvance();
-
+        for (int phase = 0; phase < INPUUT.length() / 3; phase++) {
+            new Hydrogen(Phaser);
+            new Hydrogen(Phaser);
+            new Oxygen(Phaser);
+            Phaser.arriveAndAwaitAdvance();
         }
     }
 }
+
